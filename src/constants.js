4026cc4716909a8d1fdcadb14a45275fea67bee0
keithhauzh <keithhau.thegreat@gmail.com>
2025-01-16 11:41:31 +0800

    build for deployment

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:5554";
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5555/api"
+    : "https://b13-keith24.mak3r.dev/api";
